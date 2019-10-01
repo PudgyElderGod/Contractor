2bcf45bd4d0a28ecb84d5d7cd06d8551b71b895c
PudgyElderGod <PudgyElderGod@gmail.com>
2019-10-01 16:48:08 -0700

    Users can see single playlists

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#----------Imports----------#
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -5,6 +6,7 @@ db = client.Playlister
 playlists = db.playlists
 
 from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -14,11 +16,7 @@ playlists = [
     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 ]
 
-# @app.route('/')
-# def playlists_index():
-#     """Show all playlists."""
-#     return render_template('playlists_index.html', playlists=playlists
-
+#----------Routing---------#
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
@@ -32,13 +30,20 @@ def playlists_submit():
         'description': request.form.get('description'),
         'videos': request.form.get('videos').split()
     }
-    playlists.insert_one(playlist)
-    return redirect(url_for('playlists_index'))
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html')
 
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
+
+#----------End----------#
 if __name__ == '__main__':
     app.run(debug=True)
